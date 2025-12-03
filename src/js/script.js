2b27e6552852a1cccb2d00aee178265a5a6122ec
Katya <k.dauhatseles@gmail.com>
2025-12-03 18:36:07 +0100

    Journey page initial layout add

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,44 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const dots = document.querySelectorAll(".journey__dot");
+    const panels = document.querySelectorAll(".journey__panel");
+
+    dots.forEach(dot => {
+        dot.addEventListener("click", () => {
+            const step = dot.dataset.step;
+
+            dots.forEach(d => d.classList.remove("active"));
+            dot.classList.add("active");
+
+            panels.forEach(panel => {
+                panel.classList.toggle("active", panel.dataset.step === step);
+            });
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("journeyModal");
+    const section = document.querySelector(".journey");
+    
+    if (!modal || !section) return;
+    let shown = false;
+    function checkVisibility() {
+        const rect = section.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+        const sectionHeight = section.offsetHeight;
+        const scrolled = windowHeight - rect.top; 
+        const progress = scrolled / sectionHeight; 
+
+        if (progress > 0.7 && !shown) {
+            modal.classList.add("show");
+            shown = true;
+        }
+    }
+    window.addEventListener("scroll", checkVisibility);
+});
+
+
